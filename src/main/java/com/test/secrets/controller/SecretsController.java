06e377e0164ffcc40c559a18055975b6d97b3598
mohanakrishnanv3 <104478565+mohanakrishnanv3@users.noreply.github.com>
2022-04-27 11:13:17 +0530

    Update SecretsController.java

--- src/main/java/com/test/secrets/controller/SecretsController.java
+++ src/main/java/com/test/secrets/controller/SecretsController.java
@@ -9,6 +9,11 @@ public class SecretsController {
 	@GetMapping("/test")
 	public String test()
 	{
+		Map<String, String> env = System.getenv();
+		for (Map.Entry<String, String> i : env.entrySet())
+		{
+		    System.out.println(i.getKey() +" : "+i.getValue());
+		}
 		return "Testing";
 	}
 
