f7f76c3d02cca1d17f84f3d5271df51ece262e24
mohanakrishnanv3 <104478565+mohanakrishnanv3@users.noreply.github.com>
2022-05-10 16:31:20 +0530

    Update SecretsController.java

--- src/main/java/com/test/secrets/controller/SecretsController.java
+++ src/main/java/com/test/secrets/controller/SecretsController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecretsController {
 	
+	@Autowired
+	ConfigurableEnvironment environment;
+	
+	private static final String DS_PWD = "spring.datasource.password";
+	private static final String SECRET_DS_PSWD = "SPRING_DATASOURCE_PSWD";
+	
 	@GetMapping("/test")
 	public String test()
 	{
@@ -15,6 +21,11 @@ public class SecretsController {
 		{
 		    System.out.println(i.getKey() +" : "+i.getValue());
 		}
+		
+		System.out.println("Datasource from secrets: "+environment.getProperty(SECRET_DS_PSWD));
+    	
+    	System.out.println("Datasource from  configmap: "+environment.getProperty(DS_PWD));
+		
 		return "Testing";
 	}
 
